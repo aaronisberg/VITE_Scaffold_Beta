47f0b57b48d9ee330452900076af9a17c7d08e5a
aaronisberg <148402845+aaronisberg@users.noreply.github.com>
2023-11-12 15:54:55 -0800

    Terrain Added
    This version includes Terrain Height Mapping

--- main.js
+++ main.js
@@ -7,8 +7,43 @@ const engine = new BABYLON.Engine(canvas);
 const createScene = function() {
   const scene = new BABYLON.Scene(engine);
 
-  scene.createDefaultCameraOrLight(true, false, true);
-  const box = new BABYLON.MeshBuilder.CreateBox();
+    scene.createDefaultCameraOrLight(true, false, true);
+/* const box = new BABYLON.MeshBuilder.CreateBox('myBox', {
+    size: 0.1,
+  width: 2,
+  height: 0.05,
+  depth: 0.5,
+  faceColors:[
+    new BABYLON.Color4(1,0,0,1),
+    BABYLON.Color3.Green()
+  ]  }); */
+
+ // const sphere = new BABYLON.MeshBuilder.CreateSphere('mySphere',{segments:50,
+  //diameter: 0.3,
+//diameterY: 0.4}, scene);
+
+/* const ground = new BABYLON.MeshBuilder.CreateGround('', {
+  height: 10,
+  width: 10,
+  subdivisions: 5,
+  subdivisionX: 10
+});
+
+ground.material = new BABYLON.StandardMaterial();
+ground.material.wireframe = true; */
+
+ const groundFromHM = new BABYLON.MeshBuilder.CreateGroundFromHeightMap('', '/Assets/TerrainHM_2.png', {
+  height: 20,
+  width: 10,
+  subdivisions: 50,
+  maxHeight: 2,
+
+});
+groundFromHM.material = new BABYLON.StandardMaterial();
+groundFromHM.material.wireframe = true;  
+
+/*const fontData = await (await fetch('/Montsarrat_Regular.json')).json();
+const text = BABYLON.MeshBuilder.CreateText('', "My Text")*/
 
   return scene;
 }
